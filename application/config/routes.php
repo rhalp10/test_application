ced7cf3cc6046a69ac77fd21b5ffeea9aea6ec2c
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-12-20 05:42:33 +0800

    config routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,7 +51,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'user';
 $route['dashboard'] = 'user/dashboard';
-$route['account'] = 'user/dashboard';
+$route['account'] = 'account/index';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
