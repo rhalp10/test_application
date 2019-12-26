02ba69d6af534e7971cbdaddf27e8a0afd584382
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-12-26 15:53:57 +0800

    Update menu_helper.php

--- application/helpers/menu_helper.php
+++ application/helpers/menu_helper.php
@@ -2,24 +2,36 @@
 if(!defined('BASEPATH')) exit('No direct script access allowed');
 
 if(!function_exists('side_navlist')) {
-  function side_navlist($controller,$name,$link,$icon) {
-    if ($controller == $link) {
-      $active_ul_snav = "active";
-      $active_ul_snav_span = '<span class="sr-only">(current)</span>';
+  function side_navigation($navigation,$controller,$base_url) {
+    $navlink = "";
+    foreach($navigation as $key => $sidenav){
+     
+      $sidenav_label = $key;
+
+      foreach($sidenav as $key => $li){
+          $li_a[$key] = $li;
+      }
+
+      if($li_a["controller"] == $controller){
+         $sidenav_active_ul = 'active';
+         $sidenav_active_span = '<span class="sr-only">(current)</span>';
+      }
+      else{
+         $sidenav_active_ul = '';
+         $sidenav_active_span = '';
+      }
+      
+      $navlink .= '<li class="nav-item">
+              <a class="nav-link '.$sidenav_active_ul.' " href="'.$base_url.$li_a["link"].'">
+
+                <span data-feather="'.$li_a["icon"].'" class="'.$li_a["icon"].'"></span>
+                 '.$sidenav_label.' '.$sidenav_active_span.'
+              </a>
+            </li>';
       
     }
-    else{
-       $active_ul_snav = '';
-       $active_ul_snav_span = '';
-    }
-    $navlink = '<li class="nav-item">
-            <a class="nav-link '.$active_ul_snav.' " href="'.$link.'">
-         
-              <span data-feather="'.$icon.'" ></span>
-               '.$name.' '.$active_ul_snav_span.'
-            </a>
-          </li>';
     return $navlink;
+    
   }
 }
 
